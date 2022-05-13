3cd4f90eaef259bce2d28998c7aa301e1249c118
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2022-05-13 17:38:45 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -88,7 +88,7 @@ async def start(client: Client, message: Message):
                  protect_content=True
         )
         except Exception as e:
-            Logger.WARNİNG(e)
+            Logger.warn(e)
 
 @Client.on_message(filters.command('help'))
 async def help(client: Client, message: Message):
