bebe432bb75456d686e24c5e3fa5e653c09b6032
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2022-05-13 19:51:01 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -83,6 +83,7 @@ async def help(client: Client, message: Message):
 async def sezon1(client: Client, message: Message):
     try:
         sezon1 = await client.create_chat_invite_link(int(SEZON1), member_limit = 1)
+
     sezon1btn = InlineKeyboardMarkup([[InlineKeyboardButton('1. Sezon', url=sezon1.invite_link)]])
     await client.send_message(
         text="1. Sezonu izlemek için aşağıdaki butona tıkla!",
