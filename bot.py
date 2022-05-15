e8078fa03b701274fe4390d522ce62cd7e7751c4
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2022-05-15 22:50:56 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -37,6 +37,9 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                     level=logging.INFO)
 LOGGER = logging.getLogger(__name__)
 
+def is_enabled(value:str):
+    return bool(str(value).lower() in ["true", "1", "e", "d"])
+
 BOT_TOKEN: str = environ.get('BOT_TOKEN', None)
 API_ID: int = int(environ.get('API_ID', None))
 API_HASH: str = environ.get('API_HASH', None)
